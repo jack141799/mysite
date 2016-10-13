07c581e3dc53cbe1b94d4914b1f64a4e571c17f6
root <1464322296@qq.com>
2016-10-13 16:37:46 +0800

    My simple blog first commit

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+#def post_list(request):
+#    return render(request, 'blog/post_list.html', {})
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
