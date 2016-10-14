d473b7e90cd0830f966043c8ed55757eb3f4a66f
jack141799 <1464322296@qq.com>
2016-10-14 13:56:41 +0800

    add import

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,10 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 class Post(models.Model):
     author = models.ForeignKey(User)
